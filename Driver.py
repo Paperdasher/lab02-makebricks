cb68957b05634aced7ac671c600453238b0f6374
Sean Takahashi <seanyuto@gmail.com>
2025-03-08 15:51:34 -0500

    added case testing file

---
+++ Driver.py
@@ -0,0 +1,20 @@
+#Examples how to call a method from a different Python file:
+
+
+# store it in a variable
+# int x = test(1)
+
+# print it
+# print(test(2))
+
+# print it with context!
+# print("test(test(4)) =" + test(test(4)))
+
+
+# The tests should print the intended output in addition to the actual output.
+# You should also replace the placeholder text with an actual function call.
+
+# print("makeBricks(1,1,2) expected false result: " + "REPLACE THIS")
+# print("makeBricks(1,1,6) expected true  result: " + "REPLACE THIS" )
+
+
