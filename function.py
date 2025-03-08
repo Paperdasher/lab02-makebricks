cc20377b772c0625dca374f7eea0e66d436e8f03
Sean Takahashi <seanyuto@gmail.com>
2025-03-08 15:42:54 -0500

    added assignment python file

---
+++ function.py
@@ -0,0 +1,5 @@
+## Lab02- Make Bricks ##
+
+# This lab is more conceptual than hard core coding. 
+# You will have to LOGICALLY think of the solution on paper before coding, coding the solution is not the intent.
+
