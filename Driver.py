517ca7ad7560e66f92a4d337a508f783a1bcd614
Sean Takahashi <seanyuto@gmail.com>
2025-03-08 15:56:30 -0500

    added more comments

--- Driver.py
+++ Driver.py
@@ -1,14 +1,15 @@
 #Examples how to call a method from a different Python file:
 
+from function import test
 
 # store it in a variable
-# int x = test(1)
+x = test(1)
 
 # print it
-# print(test(2))
+print(test(2))
 
 # print it with context!
-# print("test(test(4)) =" + test(test(4)))
+print("test(test(4)) =" + test(test(4)))
 
 
 # The tests should print the intended output in addition to the actual output.
@@ -18,3 +19,11 @@
 # print("makeBricks(1,1,6) expected true  result: " + "REPLACE THIS" )
 
 
+# Think of all the different possible cases, including edge cases
+# Ex. large amount of big blocks, small amount of small blocks, true: makeBricks(4, 7, 24)
+
+# This file will be checked as well to see if you are properly testing.
+# Proper testing is important to ensure...
+# 1. Your algorithm works
+# 2. You can catch edge cases
+# 3. Your code matches your algorithm
